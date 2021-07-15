e978356124bf8f15cfa7c43809bcf5aea28492e5
Julián Swerdlin <julianswer36@gmail.com>
2021-07-15 19:52:55 -0300

    updating files

--- angular-photo-gallery/src/app/app-routing.module.ts
+++ angular-photo-gallery/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PhotoListComponent} from './components/photo-list/photo-list.component'
+import {PhotoFormComponent} from './components/photo-form/photo-form.component'
+import {PhotoPreviewComponent} from './components/photo-preview/photo-preview.component'
 
 const routes: Routes = [
   {
     path: 'photos',
     component: PhotoListComponent
+  },
+  {
+    path: 'photos/new',
+    component: PhotoFormComponent
+  },
+  {
+    path: 'photos/:id',
+    component: PhotoPreviewComponent
   }
 ];
 
