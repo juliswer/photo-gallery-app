75bb1b59ba8a85b82f2a1ec52ebffc0efb56d511
Julián Swerdlin <julianswer36@gmail.com>
2021-07-15 21:18:38 -0300

    updating files

--- angular-photo-gallery/src/app/components/photo-form/photo-form.component.ts
+++ angular-photo-gallery/src/app/components/photo-form/photo-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface HtmlInputEvent extends Event {
+  target: HTMLInputElement & EventTarget
+}
+
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoFormComponent implements OnInit {
 
+  file: File;
+  photoSelected: string | ArrayBuffer;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  onPhotoSelected(event: HtmlInputEvent): void {
+    if(event.target.files && event.target.files[0]) {
+      this.file = <File>event.target.files[0];
+      // image preview
+      const reader = new FileReader();
+      reader.onload = e => this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
+  }
 
+  uploadPhoto(title: HTMLInputElement, description: HTMLTextAreaElement): boolean {
+    console.log(title.value)
+    console.log(description.value)
+    return false
+  }
 }
