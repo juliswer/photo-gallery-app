c5aabf3fdcfbf6ab6ce668887b211d8ecbfdd942
Julián Swerdlin <julianswer36@gmail.com>
2021-07-16 14:40:22 -0300

    new commit

--- angular-photo-gallery/src/app/components/photo-list/photo-list.component.ts
+++ angular-photo-gallery/src/app/components/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {PhotoService} from '../../services/photo.service'
+
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoListComponent implements OnInit {
 
-  constructor() { }
+  photos = [];
+
+  constructor(private photoService: PhotoService) { }
 
   ngOnInit(): void {
+    this.photoService.getPhotos()
+      .subscribe(
+        res => {
+          this.photos = res;
+        },
+        err => console.log(err)
+      )
   }
 
 }
